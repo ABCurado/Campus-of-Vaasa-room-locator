3ba541467743ca4a40c09c0f86c663930bdac9e2
TurboDemo <mikkor.ramo@gmail.com>
2016-11-15 10:00:01 +0200

    coordinates.js commented

--- HTML/js/coordinates.js
+++ HTML/js/coordinates.js
@@ -1,62 +1,37 @@
  $("#map").click(function(e){
 
+// Get bounding rectangle, bounding rectangle gets top left corner of the img element
   var rect = this.getBoundingClientRect();
 
-  
-  
+	//page scroll offset. If page is not scrolled value is 0
   var scrollTop = window.pageYOffset;
   var scrollLeft = window.pageXOffset;
   
-  console.log("TOP:" + scrollTop + "LEFT:" +scrollLeft);
-  
+  /*
+  * Position where pointer is clicked in map, 
+  *	Bounding rectangle and page scroll offset are substracted from it to position marker correct on map
+  */
   var posX = e.pageX - Math.round(rect.left) - scrollLeft;
   var posY = e.pageY - Math.round(rect.top) - scrollTop;
-  
-  console.log("TOpp:" + posY + "LEFtt:" +	posX);
-  
-  
-  
-  
+ 
+	// Get element height and width
   var mapWidth = $(this).width();
   var mapHeight = $(this).height();
 
-  
+  // convert position in img to percents
   var Xperc = (posX / mapWidth);
   var Yperc = (posY / mapHeight);
   
-  
+  // display coordinates
   document.getElementById("xcord1").innerHTML = "X: " + Xperc.toFixed(2);
   document.getElementById("ycord1").innerHTML = "Y: " + Yperc.toFixed(2);
   
+  // convert from 0-1 to 0-100
   Xperc = Xperc * 100;
   Yperc = Yperc * 100;
   
+  // make mark visible and give position
   document.getElementById("mark").style.left = Xperc +"%";
   document.getElementById("mark").style.top = Yperc +"%";
   document.getElementById("mark").style.visibility = "visible";
-});
-
-$("#map2").click(function(e){
-
-  var rect = this.getBoundingClientRect();
-  console.log(rect.top, rect.left);
-  
-  var posX = e.pageX - Math.round(rect.left);
-  var posY = e.pageY - Math.round(rect.top);
-  
-  console.log(posX, posY);
-  
-  
-  
-  var mapWidth = $(this).width();
-  var mapHeight = $(this).height();
-  console.log(mapWidth, mapHeight);
-  
-  var Xperc = (posX / mapWidth);
-  var Yperc = (posY / mapHeight);
-  
-  console.log(Xperc, Yperc);
-  
-  document.getElementById("xcord2").innerHTML = "X: " + Xperc.toFixed(2);
-  document.getElementById("ycord2").innerHTML = "Y: " + Yperc.toFixed(2);
 });
