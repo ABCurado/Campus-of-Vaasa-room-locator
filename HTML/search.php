f2a858bc630a95d71bc21d6e4d727faee28d3498
Antti Leppälä <antti.leppaelae@gmail.com>
2016-12-12 21:31:16 +0200

    Returns every room, but suggestions not restricted

--- HTML/search.php
+++ HTML/search.php
@@ -10,17 +10,14 @@ $q=$_GET["q"];
 //lookup all links from the xml file if length of q>0
 if (strlen($q)>0) {
   $hint="";
-  for($i=0; $i<10; $i++) {
+  for($i=0; $i<$x -> length; $i++) {
 	$y=$x->item($i)->getElementsByTagName('roomid');
 	//$z=$x->item($i)->getElementsByTagName('organization');
     if ($y->item(0)->nodeType==1) {
       //find a link matching the search text
       if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q)) {
         if ($hint=="") {
-          $hint="<a href='map.html?roomid=" .
-          $y->item(0)->childNodes->item(0)->nodeValue .
-	  "'>" .
-          $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+          $hint="<a href='map.html?roomid=" .$y->item(0)->childNodes->item(0)->nodeValue . "'>" . $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
         } 
 		
 		
